3f01b9624132bc026cb669c96cc03066763bb1ff
MikyMikyMiky <mike1024b@mail.ru>
2020-12-20 18:59:42 +0300

    Задача 16

--- zadacha18.jl
+++ zadacha18.jl
@@ -13,14 +13,3 @@ function mark_angles(r)
         moves!(r,side,n)
     end
 end
-
-function moves!(r,side)
-    num_steps=0
-    while isborder(r,side)==false
-        move!(r,side)
-        num_steps+=1
-    end
-    return num_steps
-end
-
-moves!(r,side,num_steps) = for _ in 1:num_steps move!(r,side) end
